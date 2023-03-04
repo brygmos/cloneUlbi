2590c4ee1832846106f1cfb7540308eb74d4c5ba
brygmos <daniel-best@yandex.ru>
2023-03-04 15:05:47 +0500

    test

---
+++ src/users/index.js
@@ -0,0 +1,11 @@
+import About from "../pages/About";
+import Posts from "../pages/Posts";
+import PostIdPage from "../pages/PostIdPage";
+import Login from "../pages/Login";
+import Profile from "../pages/Profile"
+
+
+export const users = [
+    {id: '0', login: 'admin', password: 'admin', exact: true},
+    {id: '0', login: 'brygmos', password:'', exact: true},
+]
